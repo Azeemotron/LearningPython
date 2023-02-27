ddf544f4f51ff97d6f8455c26cd32d11e7412f97
Azeemotron <45295286+Azeemotron@users.noreply.github.com>
2023-02-27 21:01:56 +0000

    Day 3 Completed
    All exercises & day's project were completed successfully.

---
+++ Day 3/Project/Teasure Island.py
@@ -0,0 +1,47 @@
+# Day 2 - Tip Calculator: End Project
+# Exercise Completed & Checked Against Solution
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+action = input("You find yourself at a crossroad, where do you want to go, Left or Right? ").lower()
+
+if action == "left":
+    action = input("You come to a lake. There is an island at the centre.Do you swim towards it or wait for a ship. Swim or Wait? ").lower()
+    if action == "wait":
+        action = input("You arrive at the island unharmed. You see a house with three doors. Which door do you choose to open? Red, Yellow or Blue? ").lower()
+        if action == "red":
+            print("Burned by fire. Game Over.")
+        elif action == "yellow":
+            print("You Win!")
+        elif action == "blue":
+            print("Eaten by beasts. Game Over.")
+        else:
+            print("Game Over.")
+    else:
+        print("Attacked by trout. Game Over.")
+
+else:
+    print("Fall into a hole. Game Over")
