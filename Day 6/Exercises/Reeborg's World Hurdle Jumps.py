6d83e748669279ae0fb6cabe750f99b97a154724
Azeemotron <45295286+Azeemotron@users.noreply.github.com>
2023-03-02 18:44:53 +0000

    Day 6 Start

---
+++ Day 6/Exercises/Reeborg's World Hurdle Jumps.py
@@ -0,0 +1,24 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# A challenge on Reborg's World. turn_left & move are already pre-defined.
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+for jumps in range(6):  # Instructs the Jump function to be executed 6 times (enough to get to desired position)
+    jump()
+    
